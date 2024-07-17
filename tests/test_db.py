a9bca139b80b7cce17c070cc1bee3802276a7247
Vuong Ho <hochivuong2002@gmail.com>
2024-07-17 13:32:42 -0400

    Add integration tests for homepag eand timeline

--- tests/test_db.py
+++ tests/test_db.py
@@ -5,7 +5,7 @@ from app import TimelinePost
 
 MODELS = [TimelinePost]
 
-test_db = SqliteDatabase(':memory:')
+test_db = SqliteDatabase(":memory:")
 
 class TestTimelinePost(unittest.TestCase):
 	def setUp(self):
@@ -19,16 +19,16 @@ class TestTimelinePost(unittest.TestCase):
 		test_db.close()
 
 	def test_timeline_post(self):
-		first_post = TimelinePost.create(name='Matthew Chang', email='matthewchang@gmail.com', content='Hello world, Matthew!')
+		first_post = TimelinePost.create(name="Matthew Chang", email="matthewchang@gmail.com", content="Hello world, Matthew!")
 		assert first_post.id == 1
-		second_post = TimelinePost.create(name='Vuong Ho', email='vuongho@gmail.com', content='Hello world, Vuong!')
+		second_post = TimelinePost.create(name="Vuong Ho", email="vuongho@gmail.com", content="Hello world, Vuong!")
 		assert second_post.id == 2
 
 		get_first_post = TimelinePost.get_by_id(1)
 
-		assert get_first_post.name == 'Matthew Chang'
-		assert get_first_post.email == 'matthewchang@gmail.com'
+		assert get_first_post.name == "Matthew Chang"
+		assert get_first_post.email == "matthewchang@gmail.com"
 
 		get_second_post = TimelinePost.get_by_id(2)
-		assert get_second_post.name == 'Vuong Ho'
-		assert get_second_post.email == 'vuongho@gmail.com'
+		assert get_second_post.name == "Vuong Ho"
+		assert get_second_post.email == "vuongho@gmail.com"
